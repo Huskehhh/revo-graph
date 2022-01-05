ca1085f05a4def93b820a0a0261f1e8b6de90d0b
Jordyn <jordyn@husk.pro>
2022-01-05 12:24:06 +1100

    fix: use naive date time for frontend

--- ui/src/components/Gym/Gym.tsx
+++ ui/src/components/Gym/Gym.tsx
@@ -4,8 +4,8 @@ import React, {useEffect, useState} from 'react';
 import Graph from '../Graph/Graph';
 
 const GET_GRAPH_DATA = gql`
-    query GetGraphData($gymId: Int!, $sinceEpoch: Int!) {
-        revo_graph_data(where: {gym_id: {_eq: $gymId, _gt: $sinceEpoch}, epoch: {}}, order_by: {entry: desc}) {
+    query GetGraphData($gymId: Int!, $sinceEpoch: bigint!) {
+        revo_graph_data(where: {gym_id: {_eq: $gymId}, epoch: {_gt: $sinceEpoch}}, order_by: {entry: desc}) {
             count
             epoch
         }
@@ -15,34 +15,34 @@ const GET_GRAPH_DATA = gql`
     }
 `;
 
+// Set the max date to a week ago
+const sinceDate = new Date(Date.now() - (7 * 24 * 60 * 60 * 1000));
+const naiveDatetime = sinceDate.getTime().toString().substring(0, 10);
+
 export interface RevoGraphData {
     count: number;
     epoch: number;
-};
+}
 
 export interface RevoGymData {
     name: string;
-};
+}
 
 export interface GraphReponseData {
     revo_graph_data: RevoGraphData[],
     revo_gyms: RevoGymData[],
-};
+}
 
 export interface GymProps {
     gymId: number,
-};
+}
 
 export default function Gym(props: GymProps): JSX.Element {
-    // Set the max date to a week ago
-    const sinceDate = new Date();
-    sinceDate.setDate(sinceDate.getDate() - 7);
-
     const [graphData, setGraphData] = useState(null);
     const {loading, error, data} = useQuery(GET_GRAPH_DATA, {
         variables: {
             gymId: props.gymId,
-            sinceEpoch: sinceDate.getTime()
+            sinceEpoch: naiveDatetime,
         }
     });
 
